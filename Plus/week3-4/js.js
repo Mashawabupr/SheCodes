581c4e90ac8d591b564c82341bd5f7331aabd31b
Mashawabupr <marikanik1999@gmail.com>
2022-06-09 12:50:55 +0300

    js

---
+++ Plus/week3-4/js.js
@@ -0,0 +1,26 @@
+//challenge1
+let click = document.querySelector("#special-button");
+function func() {
+  alert("Hooray!");
+}
+
+click.addEventListener("click", func);
+//challenge2
+let save = document.querySelector("#password-form");
+function func2(event) {
+  event.preventDefault();
+  let password = document.querySelector("#password-input");
+  alert(password.value);
+}
+save.addEventListener("submit", func2);
+
+//chalenge3
+let all = document.querySelector("#signup-form");
+function together(event) {
+  event.preventDefault();
+  let email = document.querySelector("#email-input");
+  let username = document.querySelector("#username-input");
+  alert(email.value);
+  alert(username.value);
+}
+all.addEventListener("submit", together);
